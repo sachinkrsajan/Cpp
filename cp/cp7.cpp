616f1a68b5f25ed7b4ac5e5fe21e14d3207dc72c
sachinkrsajan <sachinkrsajan@gmail.com>
2026-02-06 23:09:36 +0530

    cp

---
+++ cp/cp7.cpp
@@ -0,0 +1,26 @@
+//580
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            arr[i]=x;
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if (arr[i]>=1000){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+}
