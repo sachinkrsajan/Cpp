5e97a8764f3fe013825dc395ac01ba3278495c35
sachinkrsajan <sachinkrsajan@gmail.com>
2026-07-15 23:46:36 +0530

    cp

---
+++ cp/2149A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int zero=0;
+        int np=0;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            if(x==0){
+                zero++;
+            }
+            else if(x==-1){
+                np++;
+            }
+        }
+        cout<<(2*np)+zero<<endl;
+    }
+
+    return 0;
+}
