ccecf28c52a56203cc99b8014d9d8c414f824543
sachinkrsajan <sachinkrsajan@gmail.com>
2026-07-25 23:34:58 +0530

    cp

---
+++ cp/1676A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s[0]-'0'+s[1]-'0'+s[2]-'0'==s[3]-'0'+s[4]-'0'+s[5]-'0'){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+
+    return 0;
+}
