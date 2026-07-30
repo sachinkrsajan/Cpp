5b2034cd8526fb125b04d7901feb21dab2ac9db2
sachinkrsajan <sachinkrsajan@gmail.com>
2026-07-30 22:57:37 +0530

    cp

---
+++ cp/2145A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%3==0){
+            cout<<0<<endl;
+        }
+        else if(n%3==1){
+            cout<<2<<endl;
+        }
+        else{
+            cout<<1<<endl;
+        }
+    }
+
+    return 0;
+}
